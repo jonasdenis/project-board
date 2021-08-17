59c21e2e473e93dcd5225426721871d0bfcb920a
Jonas Denis <jonas.denis@adesso.de>
2021-08-17 11:25:28 +0200

    reorderd project-structure for dep-check

--- frontend/project-board-app/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  authHost: 'http://10.1.101.159:8181',
-  resourceServer: 'http://10.1.101.159:8180',
-  supportEmail: 'support.projectboard@adesso.de'
-};
